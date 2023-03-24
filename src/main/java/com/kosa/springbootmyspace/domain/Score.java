23d05aab9e776b2444ab897d0e31d761a4cd7978
jina <dhaud42@naver.com>
2023-03-24 17:39:43 +0900

    ㅋ

--- src/main/java/com/kosa/springbootmyspace/domain/Score.java
+++ src/main/java/com/kosa/springbootmyspace/domain/Score.java
@@ -1,13 +1,6 @@
 package com.kosa.springbootmyspace.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -40,4 +33,7 @@ public class Score {
     @JoinColumn(name = "review_idx")
     @JsonIgnore
     private Review review;
+
+    @Transient
+    private double total;
 }
