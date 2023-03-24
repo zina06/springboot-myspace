c3c102cb59acd6b67425639adf7b5af82169a5aa
LEE <evisahah@gmail.com>
2023-03-24 21:45:16 +0900

    woo

--- src/main/java/com/kosa/springbootmyspace/domain/Member.java
+++ src/main/java/com/kosa/springbootmyspace/domain/Member.java
@@ -1,15 +1,11 @@
 package com.kosa.springbootmyspace.domain;
 
 import java.util.Date;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 import org.springframework.format.annotation.DateTimeFormat;
