657ed577f0277b63ec8fdd6bf373c6bac349dd37
jina <dhaud42@naver.com>
2023-03-24 16:32:31 +0900

    product controller CRUD 완려ㅛ

--- src/main/java/com/kosa/springbootmyspace/web/ProductController.java
+++ src/main/java/com/kosa/springbootmyspace/web/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/product")
@@ -17,6 +19,49 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        try {
+            Product saveProduct = productService.save(product);
+            if (saveProduct != null) {
+                return new ResponseEntity<Product>(saveProduct, HttpStatus.CREATED);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new ResponseEntity<Product>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<Product> update(@RequestBody Product product) {
+        try {
+            int result = productService.update(product);
+            if (result == 1) {
+                Product updateProduct=productService.findById(product.getIdx());
+                return new ResponseEntity<Product>(updateProduct, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new ResponseEntity<Product>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @DeleteMapping("/delete/{idx}")
+    public ResponseEntity<Integer> delete(@PathVariable int idx) {
+        try {
+            int result = productService.delete(idx);
+            if (result == 1) {
+                return new ResponseEntity<Integer>(result, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new ResponseEntity<Integer>(HttpStatus.NO_CONTENT);
+    }
+
 
     @GetMapping("/{idx}")
     public ResponseEntity<Product> findById(@PathVariable int idx) {
@@ -29,5 +74,18 @@ public class ProductController {
             log.error(e.getMessage());
         }
         return new ResponseEntity<Product>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Product>> findAll() {
+        try {
+            List<Product> productList = productService.findAll();
+            if (productList != null) {
+                return new ResponseEntity<List<Product>>(productList, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new ResponseEntity<List<Product>>(HttpStatus.NO_CONTENT);
     }
 }
