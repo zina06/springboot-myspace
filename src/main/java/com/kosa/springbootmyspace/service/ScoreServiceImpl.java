82d1d4b46d36bf4a1c77acf23da8c3553fd1db13
LEE <evisahah@gmail.com>
2023-03-27 22:39:43 +0900

    boot final

--- src/main/java/com/kosa/springbootmyspace/service/ScoreServiceImpl.java
+++ src/main/java/com/kosa/springbootmyspace/service/ScoreServiceImpl.java
@@ -10,25 +10,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ScoreServiceImpl implements  ScoreService{
+public class ScoreServiceImpl implements ScoreService {
     @Autowired
     private ScoreRepository scoreRepository;
 
     @Override
     public Score save(Score score) {
-        score.setTotal((score.getDelivery()+score.getPrice()+score.getDesign()+ score.getPrice())/4f);
+        score.setTotal((score.getDelivery() + score.getPrice() + score.getDesign() + score.getPrice()) / 4f);
         return scoreRepository.save(score);
     }
+
     @Override
     public int update(Score score) {
         Optional<Score> findScore = scoreRepository.findById(score.getIdx());
-        int result=0;
-        if(findScore.isPresent()){
+        int result = 0;
+        if (findScore.isPresent()) {
             Score dbScore = findScore.get();
             dbScore.setDurability(score.getDurability());
             dbScore.setPrice(score.getPrice());
             dbScore.setDesign(score.getDesign());
             dbScore.setReview(new Review().getScore().getReview());
+            dbScore.setTotal((score.getDelivery() + score.getPrice() + score.getDesign() + score.getPrice()) / 4f);
             scoreRepository.save(dbScore);
             result = 1;
         }
@@ -40,7 +42,7 @@ public class ScoreServiceImpl implements  ScoreService{
     public int delete(int idx) {
         Optional<Score> findScore = scoreRepository.findById(idx);
         int result = 0;
-        if(findScore.isPresent()){
+        if (findScore.isPresent()) {
             scoreRepository.delete(findScore.get());
             return 1;
         }
